7e56a72ebb3716933fd26e249c29d0676087787b
Kiran Shila <me@kiranshila.com>
2024-03-01 15:30:43 -0800

    Update model

--- training/train.jl
+++ training/train.jl
@@ -1,12 +1,14 @@
 using Flux, CSV, CUDA, Plots, Statistics, ProgressMeter, JLD2
 
+const dir = @__DIR__
+
+include(dir * "/../src/RlgcNn.jl")
+include("evaluate.jl")
+
 using .RlgcNn
 
-include("evaluate.jl")
-include("../src/RlgcNn.jl")
-
 # Prepare training data
-file = CSV.File("data/HFSS_2D_SS_RLGC.csv")
+file = CSV.File(dir * "/../data/HFSS_2D_SS_RLGC.csv")
 
 x = vcat(file["width_mm"]', file["freq_ghz"]')
 y = vcat(file["r_ohm"]', file["l_uH"]', file["g_uS"]', file["c_pf"]')
@@ -36,5 +38,8 @@ plot(z0_error; ylims=(0, 20), label="Z₀ %-Error")
 plot!(gamma_error, label="Γ %-Error")
 last(z0_error)
 
+# Turn off learning so we can use it in other AD contexts
+testmode!(model)
+
 state = Flux.state(cpu(model))
-jldsave("data/ss_model.jld2"; state)
+jldsave(dir * "/../data/ss_model.jld2"; state)
